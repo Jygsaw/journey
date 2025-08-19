d9ec855b0cdc05f73e6add14738ea092f0d7c11e
Justin Cheung <jygsaw@gmail.com>
2025-08-19 16:12:46 -0700

    [NEW] create UserContext

---
+++ src/components/GlobalContext.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Auth } from '@/types';
+import { UserContext } from '@/contexts/UserContext';
+
+interface InputProps {
+  auth: Auth,
+}
+
+export const GlobalContext = ({ auth, children }: InputProps) => {
+  return (
+    <UserContext value={auth.user}>
+      {children}
+    </UserContext>
+  );
+};
